23076392a5948cb8a42b25f5958eaaa68b81d754
binhu01 <bin.hu@armchina.com>
2021-11-01 15:23:08 +0800

    code is ready waiting for test

--- vector.c
+++ vector.c
@@ -5,7 +5,16 @@
 
 #define VECTOR_ELEMENT(v, i) ((void *) ((char *) (v)->mem + (i) * (v)->object_size))
 
-vector* vector_create(const size_t object_size)
+struct _vector
+{
+    size_t element_num;
+    size_t capacity;
+    size_t object_size;
+    void* mem;
+    vector_deleter* deleter;
+};
+
+vector* vector_create(const size_t object_size, vector_deleter* deleter)
 {
     vector* v = (vector*)malloc(sizeof(vector));
 
@@ -14,6 +23,7 @@ vector* vector_create(const size_t object_size)
     v->element_num = 0;
     v->object_size = object_size;
     v->capacity = sizeof(size_t);
+    v->deleter = deleter;
     v->mem = malloc(v->capacity * object_size);
     if (v->mem == NULL)
     {
@@ -24,10 +34,13 @@ vector* vector_create(const size_t object_size)
     return v;
 }
 
-void vector_destroy(vector* v)
+void vector_release(vector* v)
 {
     if(!v)
         return;
+    if(v->deleter)
+        for (size_t i = 0; i < v->element_num; v++)
+            v->deleter(vector_at(v, i));
     free(v->mem);
     free(v);
 }
@@ -87,24 +100,76 @@ bool vector_push_back(vector* v, const void* data)
 
 bool vector_pop_back(vector* v)
 {
-    //if(v->deleter)
-    //    v->deleter(vector_back(v));
+    assert(v != NULL);
+    assert(v->element_num >= 1);
+    if(v->deleter)
+        v->deleter(vector_back(v));
     v->element_num--;
     return true;
 }
 
-void* get_vector_data(const vector* v, const size_t idx)
+bool vector_pop_back_no_delete(vector* v)
 {
     assert(v != NULL);
-    assert(v->mem != NULL);
-
-    if (idx >= v->element_num) {
-            return NULL;
-    }
-
-    return VECTOR_ELEMENT(v, idx);
+    assert(v->element_num >= 1);
+    v->element_num--;
+    return true;
 }
 
+bool vector_insert(vector* v, size_t idx, const void* data)
+{
+    assert(v != NULL);
+        
+    if(v->element_num == v->capacity && vector_resize(v, v->element_num + 1) == false)
+        return false;
+
+    if (!memmove(vector_at(v, idx + 1),
+                 vector_at(v, idx),
+                 v->object_size * (v->element_num - idx))) 
+        return false;
+
+    if (memcpy(vector_at(v, idx),
+                      data,
+                      v->object_size) == NULL) 
+        return false;
+
+    v->element_num++;
+
+    return true;
+}
+
+bool vector_erase(vector* v, size_t idx)
+{
+    if (v->deleter) {
+        v->deleter(vector_at(v, idx));
+    }
+
+    if (!memmove(vector_at(v, idx), vector_at(v, idx + 1),
+                        v->object_size * (v->element_num - idx))) 
+    return false;
+
+    v->element_num--;
+    return true;
+}
+
+bool vector_erase_range(vector* v, size_t first_idx, size_t last_idx)
+{
+    if (v->deleter != NULL) 
+        for (size_t i = first_idx; i < last_idx; ++i)
+            v->deleter(vector_at(v, i));
+
+    if (!memmove(vector_at(v, first_idx),
+                 vector_at(v, last_idx),
+                 v->object_size * (v->element_num - last_idx))) 
+        return false;
+
+    v->element_num -= last_idx - first_idx;
+    
+    return true;
+
+}
+
+//Element access
 void* vector_front(const vector* v)
 {
     assert(v != NULL);
@@ -126,6 +191,24 @@ void* vector_back(const vector* v)
     }
     return (void *)((char *)(v->mem + (v->element_num - 1) * (v->object_size)));
 }
+
+void* get_vector_data(const vector* v, const size_t idx)
+{
+    assert(v != NULL);
+    assert(v->mem != NULL);
+
+    if (idx >= v->element_num) {
+            return NULL;
+    }
+
+    return VECTOR_ELEMENT(v, idx);
+}
+
+void* vector_at(const vector* v, size_t idx)
+{
+    return get_vector_data(v, idx);
+}
+
 
 size_t get_vector_size(const vector* v)
 {
@@ -156,4 +239,26 @@ void* vector_end(const vector* v)
 void* vector_next(const vector* v, const void* i)
 {
     return (char*)i + v->object_size;
+}
+
+
+bool vector_empty(vector* v)
+{
+    assert(v != NULL);
+	assert(v->mem != NULL);
+
+	v->element_num == 0;
+
+    return true;
+}
+
+bool vector_clear(vector* v)
+{
+    if (!v) 
+        return false;
+    if (v->deleter)
+        for (size_t i = 0; i < v->element_num; v++)
+            v->deleter(vector_at(v, i));
+    v->element_num = 0;
+    return true;
 }
--- vector.h
+++ vector.h
@@ -4,26 +4,19 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-typedef struct _vector
-{
-    size_t element_num;
-    size_t capacity;
-    size_t object_size;
-    void* mem;
-}vector;
+typedef void (vector_deleter)(void *);
+typedef struct _vector vector;
 
-vector* vector_create(const size_t object_size);
-void vector_destroy(vector* vector);
+//base
+vector* vector_create(const size_t object_size, vector_deleter* deleter);
+void vector_release(vector* vector);
 
-bool vector_push_back(vector* vector, const void* data);
-bool vector_pop_back(vector* vector);
-bool vector_push_back_no_copy(const vector* vector, void* data);
-bool vector_pop_back_no_delete(const vector* vector);
-
+//Element access
 void* vector_front(const vector* vector);
 void* vector_back(const vector* vector);
-
+void* vector_at(const vector* vector, size_t idx);
 void* get_vector_data(const vector* vector, const size_t idx);
+
 size_t get_vector_size(const vector* vector);
 size_t get_vector_capacity(const vector* vector);
 
@@ -31,5 +24,19 @@ size_t get_vector_capacity(const vector* vector);
 void* vector_begin(const vector* vector);
 void* vector_end(const vector* vector);
 void* vector_next(const vector* vector, const void* i);
+
+//modifiers
+bool vector_push_back(vector* vector, const void* data);
+bool vector_pop_back(vector* vector);
+bool vector_push_back_no_copy(const vector* vector, void* data);
+bool vector_pop_back_no_delete(vector* vector);
+bool vector_insert(vector* vector, size_t idx, const void* data);
+bool vector_erase(vector* vector, size_t idx);
+bool vector_erase_range(vector* vector, size_t first_idx, size_t last_idx);
+
+
+/* Return `true` if `v` is empty; otherwise, return `false`. */
+bool vector_empty(vector* v);
+bool vector_clear(vector* v);
 
 #endif
